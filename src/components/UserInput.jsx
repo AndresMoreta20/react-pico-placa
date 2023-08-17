8e677c2ecdfb0ab349547acaa468f4d467fd1e94
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-08-17 08:19:17 -0500

    Add validations to user input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,23 +1,57 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useController } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z, string } from "zod";
+
+
+
+
 
 function UserInput() {
-  const { register, control } = useForm();
 
-  return(
+  const schema = z.object({
+    plate:string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    }),
+    date:string(),
+    time:string()
+  })
 
-  <form className="form">
-    <label className="label">Let's check your plate</label>
-    <input className="input" name="plate" ></input>
-    <div style={{ color: "red" }}></div>
-    <label className="label">Type a specific date</label>
-      <input className="input" name="date"></input>
-      <div style={{ color: "red" }}></div>
+
+
+  const { register, handleSubmit, formState} = useForm({
+    defaultValues: {
+      plate: "",
+      date: "",
+      time: "",
+    }
+    , resolver:zodResolver(schema) 
+  });
+
+  const {errors} = formState;
+  //const {field} = useController({})
+
+  const handleSave = (formValues) => {
+    console.log(formValues);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit(handleSave)}>
+      <label className="label">Let's check your plate</label>
+      <input className="input" name="plate" {...register("plate")}></input>
+      <div style={{ color: "red" }}>{errors.plate?.message}</div>
+
+      <label className="label">Type a specific date</label>
+      <input className="input" name="date" {...register("date")}></input>
+      <div style={{ color: "red" }}>{errors.date?.message}</div>
+
       <label className="label">What time?</label>
-      <input className="input" name="time"></input>
-      <div style={{ color: "red" }}></div>
-    <button type="submit">Check</button>
-  </form>);
+      <input className="input" name="time" {...register("time")}></input>
+      <div style={{ color: "red" }}>{errors.time?.message}</div>
+      <button type="submit">Check</button>
+    </form>
+  );
 }
 
 export default UserInput;
