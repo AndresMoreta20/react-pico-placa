7dce93f6228c7631a2d55a39dd8682be39656965
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-08-17 12:06:33 -0500

    Refactor input validation and enhance form style

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,22 +3,22 @@ import { useForm, useController } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z, string } from "zod";
 
-
-
-
+//Generic inputfield that includes its validation message
+const InputField = ({ label, name, type = "text", register, errors }) => (
+  <div>
+    <label className="label">{label}</label>
+    <input className="input" name={name} type={type} {...register(name)} />
+    <div style={{ color: "red" }}>{errors[name]?.message}</div>
+  </div>
+);
 
 function UserInput() {
-
+//ZOD is used for validation and each instance must have custom messages
   const schema = z.object({
-    plate:string({
-      required_error: "Name is required",
-      invalid_type_error: "Name must be a string",
-    }),
-    date:string(),
-    time:string()
+    plate:string().min(1,{message:"The plate is required"}).regex(/^[a-zA-Z]{3}-\d{4}$/, {message:"Incorrect format"}),
+    date:string().min(1,{message:"Date is required"}).regex(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/, {message:"The date must be in this format: 'DD/MM/YYYY'"}),
+    time:string().min(1,{message:"Time is required"})
   })
-
-
 
   const { register, handleSubmit, formState} = useForm({
     defaultValues: {
@@ -30,25 +30,19 @@ function UserInput() {
   });
 
   const {errors} = formState;
-  //const {field} = useController({})
 
   const handleSave = (formValues) => {
-    console.log(formValues);
+     console.log(formValues);
   };
 
   return (
     <form className="form" onSubmit={handleSubmit(handleSave)}>
-      <label className="label">Let's check your plate</label>
-      <input className="input" name="plate" {...register("plate")}></input>
-      <div style={{ color: "red" }}>{errors.plate?.message}</div>
+      <InputField label="Let's check your plate" name="plate" register={register} errors={errors} />
 
-      <label className="label">Type a specific date</label>
-      <input className="input" name="date" {...register("date")}></input>
-      <div style={{ color: "red" }}>{errors.date?.message}</div>
+      <InputField label="Type a specific date" name="date" register={register} errors={errors} />
 
-      <label className="label">What time?</label>
-      <input className="input" name="time" {...register("time")}></input>
-      <div style={{ color: "red" }}>{errors.time?.message}</div>
+      <InputField label="Pick a time" name="time" type="time" register={register} errors={errors} />
+
       <button type="submit">Check</button>
     </form>
   );
